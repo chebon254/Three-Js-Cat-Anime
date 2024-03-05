e9f454309e7247aa7c7ab3117df69a0bf592564c
chebon254 <kelvinchebon90@gmail.com>
2024-03-05 15:07:00 +0300

    Cat Build

--- threejs/main.js
+++ threejs/main.js
@@ -37,7 +37,7 @@ const loader = new GLTFLoader();
 
 //Load the file
 loader.load(
-  `/${objToRender}/cat.gltf`,
+  `/${objToRender}/cat2_rig03.gltf`,
   function (gltf) {
     //If the file is loaded, add it to the scene
     object = gltf.scene;
